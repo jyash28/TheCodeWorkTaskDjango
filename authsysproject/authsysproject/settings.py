a59d4bcfb77234554c9d942a24aab18864e49818
yash jangir <jyash548@gmail.com>
2023-04-04 12:36:09 +0530

    changes

--- authsysproject/authsysproject/settings.py
+++ authsysproject/authsysproject/settings.py
@@ -168,5 +168,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yash.jangir@consolebit.com'
+EMAIL_HOST_USER = 'your mail id'
 EMAIL_HOST_PASSWORD = 'your app password'
