01ac7a6319e1f5005aa4de7f0cebebef6a2d548a
yash jangir <jyash548@gmail.com>
2023-04-04 11:52:59 +0530

    change app password

--- authsysproject/authsysproject/settings.py
+++ authsysproject/authsysproject/settings.py
@@ -169,4 +169,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yash.jangir@consolebit.com'
-EMAIL_HOST_PASSWORD = 'atgycsyjaeqeesiw'
+EMAIL_HOST_PASSWORD = 'your app password'
